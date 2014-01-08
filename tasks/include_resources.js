38abceaa2eba7f5cdeda2dadd88a7b047838b394
Denis Suckau <denis.suckau@concepts-and-training.de>
2014-01-08 17:11:40 +0100

    replace include directives

--- tasks/include_resources.js
+++ tasks/include_resources.js
@@ -12,16 +12,101 @@ module.exports = function (grunt) {
 
     grunt.registerMultiTask('include_resources', 'Include resources into HTML files.', function() {
 
+        // Default options
         var options = this.options({
                 prefix: '<!-- include: ',
-                suffix: ' -->'
+                suffix: ' -->',
+                css: {},
+                js: {}
             }),
-            pattern = new RegExp(
+
+            // Regular expression to match include directives
+            re = new RegExp(
                 options.prefix +
-                '' +
+                '({[\\s\\S]*?})' +
                 options.suffix,
                 'g'
             );
+
+        this.files.forEach(function (file) {
+
+            // Merge all source files
+            var src = file.src.filter(function (filepath) {
+                    if (!grunt.file.exists(filepath)) {
+                        grunt.log.warn('Source file "' + filepath + '" not found.');
+                        return false;
+                    }
+                    return true;
+                })
+                .map(grunt.file.read)
+                .join(grunt.util.normalizelf(grunt.util.linefeed)),
+                includes = [],
+                match;
+
+            // Match all include directives
+            while (match = re.exec(src)) {
+                includes.push({
+                    directive: match[0],
+                    config: JSON.parse(match[1])
+                });
+            }
+
+            includes.forEach(function (include) {
+
+                var id = include.config.id,
+                    type = include.config.type,
+                    data = '',
+                    filepath,
+                    content;
+
+                // Identifier found in options
+                if (id in options[type]) {
+
+                    filepath = options[type][id];
+
+                    // Resource file found
+                    if (grunt.file.exists(filepath)) {
+
+                        // Read resource file content
+                        content = grunt.file.read(filepath);
+
+                        // JavaScript
+                        if (type === 'js') {
+                            data = [
+                                '<script type="text/javascript">',
+                                content,
+                                '</script>'
+                            ].join(grunt.util.normalizelf(grunt.util.linefeed));
+
+                        // CSS
+                        } else {
+                            data = [
+                                '<style type="text/css">',
+                                content,
+                                '</style>'
+                            ].join(grunt.util.normalizelf(grunt.util.linefeed));
+                        }
+
+                    // Resource file not found
+                    } else {
+                        grunt.log.warn('Resource file "' + filepath + '" not found.');
+                    }
+
+                // Identifier not found in options
+                } else {
+                    grunt.log.warn('Identifier "' + id + '" with type "' + type + '" not found.');
+                }
+
+                // Replace include directive
+                src = src.replace(include.directive, data);
+
+            });
+
+            // Write destination file
+            grunt.file.write(file.dest, src);
+            grunt.log.writeln('File "' + file.dest + '" created.');
+
+        });
 
     });
 
